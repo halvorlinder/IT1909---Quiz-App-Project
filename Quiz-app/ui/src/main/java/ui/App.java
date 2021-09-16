b5bd9a09d90fe9d09370b24c57fc6614ef7de078
oskarjor <oskarjor@stud.ntnu.no>
2021-09-16 12:52:10 +0200

    Code for starting app and switching scenes

--- Quiz-app/ui/src/main/java/ui/App.java
+++ Quiz-app/ui/src/main/java/ui/App.java
@@ -1,6 +1,8 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,12 +15,32 @@ import java.io.IOException;
  */
 public class App extends Application {
 
+    private static Stage primaryStage; // **Declare static Stage**
+
+    private void setPrimaryStage(Stage stage) {
+        App.primaryStage = stage;
+    }
+
+    static public Stage getPrimaryStage() {
+        return App.primaryStage;
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
+        setPrimaryStage(stage);
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("App.fxml"));
         Parent parent = fxmlLoader.load();
         stage.setScene(new Scene(parent));
         stage.show();
+    }
+
+    @FXML
+    public void setRoot(String FXMLFile) throws IOException { // Switches view to desired FXML file
+        Parent root = FXMLLoader.load(getClass().getResource(FXMLFile));
+        Scene scene = new Scene(root);
+        Stage primaryStage = getPrimaryStage();
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
