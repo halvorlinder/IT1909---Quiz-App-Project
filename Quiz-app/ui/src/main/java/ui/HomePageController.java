9c7702c747e273fc44cfe3ff8139b0b5a3bbeebe
oskarjor <oskarjor@stud.ntnu.no>
2021-09-17 11:34:44 +0200

    Added controller for HomePage

--- Quiz-app/ui/src/main/java/ui/HomePageController.java
+++ Quiz-app/ui/src/main/java/ui/HomePageController.java
@@ -4,11 +4,7 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 public class HomePageController {
     
