aaa9252a632079023d9b5e0979367c4c0159047e
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-10-08 15:03:37 +0200

    add QuizDeserializer

---
+++ Quiz-app/core/src/main/java/io/QuizDeserializer.java
@@ -0,0 +1,49 @@
+package io;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import core.Question;
+import core.Quiz;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+class QuizDeserializer extends JsonDeserializer<Quiz> {
+
+    private final QuestionDeserializer questionDeserializer = new QuestionDeserializer();
+
+    @Override
+    public Quiz deserialize(JsonParser parser, DeserializationContext ctx) throws IOException, JsonProcessingException {
+        TreeNode treeNode = parser.getCodec().readTree(parser);
+        return deserialize((JsonNode) treeNode);
+    }
+
+    Quiz deserialize(JsonNode jsonNode) {
+        if (jsonNode instanceof ObjectNode objectNode) {
+            JsonNode textNode = objectNode.get("name");
+            String name = textNode.asText();
+            List<Question> questionList = new ArrayList<>();
+            JsonNode questionsNode = objectNode.get("questions");
+            boolean hasQuestions = questionsNode instanceof ArrayNode;
+            if (hasQuestions) {
+                for (JsonNode questionNode : questionsNode) {
+                    Question question = questionDeserializer.deserialize(questionNode);
+                    if (question != null) {
+                        questionList.add(question);
+                    }
+                }
+            }
+            return new Quiz(name, questionList);
+
+        }
+        return null;
+    }
+}
