b90d1ea485c92f4ab5928547b242fb18a5adb4fc
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-10-13 22:40:13 +0200

    add UserPersistence

---
+++ Quiz-app/core/src/main/java/io/UserPersistence.java
@@ -0,0 +1,14 @@
+package io;
+
+public class UserPersistence {
+    public boolean addUser(String userName, String password){
+        return false;
+    }
+    public boolean userExists(String userName){
+        return false;
+    }
+
+    public boolean successfulLogIn(String userName, String password){
+        return false;
+    }
+}
