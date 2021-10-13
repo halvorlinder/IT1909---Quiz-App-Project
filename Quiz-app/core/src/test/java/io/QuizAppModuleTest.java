0ae008d2305f044a0b28e86e2259462dcea30546
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-10-13 16:35:32 +0200

    create TestHelpers

--- Quiz-app/core/src/test/java/io/QuizAppModuleTest.java
+++ Quiz-app/core/src/test/java/io/QuizAppModuleTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static io.TestHelpers.checkQuiz;
+import static io.TestHelpers.createQuizWithTwoQuestions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -37,12 +39,6 @@ public class QuizAppModuleTest {
                                  
                      """;
 
-    static Quiz createQuizWithTwoQuestions() {
-        return new Quiz("quiz101", List.of(
-                new Question("What?", List.of("a", "b", "c", "d"), 2),
-                new Question("Where", List.of("1", "2", "3", "4"), 3)));
-    }
-
     @Test
     public void testSerializers() {
         Quiz quiz = createQuizWithTwoQuestions();
@@ -50,23 +46,6 @@ public class QuizAppModuleTest {
             assertEquals(quizWithTwoQuestions.replaceAll("\\s+", ""), mapper.writeValueAsString(quiz));
         } catch (JsonProcessingException e) {
             fail(e.getMessage());
-        }
-    }
-
-    static void checkQuestion(Question question1, Question question2) {
-        assertEquals(question1.getQuestion(), question2.getQuestion());
-        assertEquals(question1.getChoices().size(), question1.getChoices().size());
-        for (int i = 0; i < question1.getChoices().size(); i++) {
-            assertEquals(question1.getChoice(i), question2.getChoice(i));
-        }
-        assertEquals(question1.getAnswer(), question2.getAnswer());
-    }
-
-    static void checkQuiz(Quiz quiz1, Quiz quiz2) {
-        assertEquals(quiz1.getName(), quiz2.getName());
-        assertEquals(quiz1.getQuizLength(), quiz2.getQuizLength());
-        for (int i = 0; i < quiz1.getQuizLength(); i++) {
-            checkQuestion(quiz1.getQuestions().get(i), quiz2.getQuestions().get(i));
         }
     }
 
