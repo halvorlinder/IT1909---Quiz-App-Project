1cf54ad99319a6480814413c50b407dfd9b68aa3
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-10-14 09:15:16 +0200

    create UserDataSerializer

---
+++ Quiz-app/core/src/main/java/io/internal/UserDataSerializer.java
@@ -0,0 +1,19 @@
+package io.internal;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import core.UserData;
+
+import java.io.IOException;
+
+public class UserDataSerializer extends JsonSerializer<UserData> {
+    @Override
+    public void serialize(UserData userData, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeStartObject();
+        for (String name : userData.getUserNames()) {
+            jsonGenerator.writeNumberField(name, userData.getPasswordHash(name));
+        }
+        jsonGenerator.writeEndObject();
+    }
+}
