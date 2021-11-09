35176fe2bf62c9594b64e226f20426bfac02d0c5
oskarjor <oskarjor@stud.ntnu.no>
2021-11-09 14:48:55 +0100

    fix: remove unused imports
    remove unused imports

--- Quiz-app/ui/src/main/java/ui/controllers/LogInController.java
+++ Quiz-app/ui/src/main/java/ui/controllers/LogInController.java
@@ -8,13 +8,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import ui.App;
 import ui.Utilities;
 
-import java.io.File;
 import java.io.IOException;
 
 public final class LogInController {
