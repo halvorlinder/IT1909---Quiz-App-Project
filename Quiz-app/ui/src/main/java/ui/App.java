44b927163671b2149dc05890748bfd9772dd1a2f
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-11-12 20:23:14 +0100

    fix: remove test code
    removed a couple of lines for testing the api-communication

--- Quiz-app/ui/src/main/java/ui/App.java
+++ Quiz-app/ui/src/main/java/ui/App.java
@@ -7,11 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ui.controllers.LogInController;
 
-import java.net.URI;
-import java.net.http.HttpClient;
 import java.io.IOException;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 /**
  * JavaFX App
@@ -20,9 +16,6 @@ public final class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
-
-        APIClientService apiClientService = new APIClientService();
-        apiClientService.getQuiz("quiz101");
         final FXMLLoader loader = Utilities.getFXMLLoader("LogInPage.fxml");
         LogInController controller = new LogInController();
         loader.setController(controller);
