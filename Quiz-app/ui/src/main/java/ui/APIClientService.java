7fdc151450b5a1b5515bd1294b9e15979e13a5f1
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-11-12 20:31:34 +0100

    refactor: imports and formatting
    fixed imports, documentations and formatting

--- Quiz-app/ui/src/main/java/ui/APIClientService.java
+++ Quiz-app/ui/src/main/java/ui/APIClientService.java
@@ -1,8 +1,10 @@
 package ui;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import core.Question;
 import core.Quiz;
+import io.QuizPersistence;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,17 +12,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
-import org.springframework.http.HttpStatus;
-
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.QuizPersistence;
 
 public class APIClientService {
 
     private static final String API_URL = "http://localhost:8080/api";
     private final HttpClient client = HttpClient.newHttpClient();
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
     /**
      *
@@ -108,14 +105,24 @@ public class APIClientService {
         sendRequest("POST", "/quizzes/" + quizName, objectMapper.writeValueAsString(newQuestion));
     }
 
-
-    private HttpResponse<String> sendRequest(String method, String relativePath, String body) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder().method(method, HttpRequest.BodyPublishers.ofString(body)).uri(URI.create(API_URL+relativePath)).build();
+    /**
+     * sends a request to the server
+     * @param method the http method
+     * @param relativePath the path relative to /api
+     * @param body the body of the request
+     * @return the response of the request
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private HttpResponse<String> sendRequest(String method, String relativePath, String body)
+            throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().method(method, HttpRequest.BodyPublishers.ofString(body))
+                .uri(URI.create(API_URL + relativePath)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         int statusCode = response.statusCode();
-        if(statusCode > 299){
-            System.out.println(HttpStatus.valueOf(statusCode).getReasonPhrase());
-            throw new IOException("Response from server is not valid. The status code is"+ statusCode);
+        if (statusCode > 299) {
+            System.out.println("Fail: " + HttpStatus.valueOf(statusCode).getReasonPhrase());
+            throw new IOException("Response from server is not valid. The status code is" + statusCode);
         }
         return response;
     }
