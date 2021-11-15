86a979ee05bbd2a002f3bdd60b611c53afaa5eec
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-11-15 14:29:16 +0100

    docs: add javadoc
    Added javadoc

--- Quiz-app/rest/src/main/java/rest/QuizController.java
+++ Quiz-app/rest/src/main/java/rest/QuizController.java
@@ -18,12 +18,21 @@ public class QuizController {
     private final QuizPersistence quizPersistence;
     private final ObjectMapper objectMapper;
 
+    /**
+     * inits the controller
+     * @throws IOException
+     */
     public QuizController() throws IOException {
         this.objectMapper = UserPersistence.createObjectMapper();
         this.quizPersistence = new QuizPersistence();
     }
 
-
+    /**
+     * returns a response containing a quiz
+     * @param name the name of the quiz
+     * @param response
+     * @return the quiz
+     */
     @GetMapping("/quizzes/{name}")
     public String getQuiz(@PathVariable("name") String name, HttpServletResponse response) {
         try {
@@ -35,6 +44,11 @@ public class QuizController {
         return null;
     }
 
+    /**
+     *
+     * @param response
+     * @return a list of all quizzes
+     */
     @GetMapping("/quizzes")
     public String getQuizzes(HttpServletResponse response) {
         try {
@@ -46,6 +60,12 @@ public class QuizController {
         return null;
     }
 
+    /**
+     * posts a new quiz
+     * @param quizJSON the new quiz
+     * @param response
+     * @return the new quiz
+     */
     @PostMapping("/quizzes")
     public String postQuiz(@RequestBody String quizJSON, HttpServletResponse response) {
         try {
@@ -63,8 +83,16 @@ public class QuizController {
         return null;
     }
 
+    /**
+     *
+     * @param question the new question
+     * @param quizName the name of the quiz
+     * @param response
+     * @return the quiz
+     */
     @PostMapping("quizzes/{name}")
-    public String addQuestion(@RequestBody String question, @PathVariable("name") String quizName, HttpServletResponse response) {
+    public String addQuestion(@RequestBody String question,
+                              @PathVariable("name") String quizName, HttpServletResponse response) {
         try {
             Quiz quiz = quizPersistence.loadQuiz(quizName);
             quiz.addQuestion(objectMapper.readValue(question, Question.class));
@@ -77,8 +105,18 @@ public class QuizController {
         return null;
     }
 
+    /**
+     *
+     * @param question a new question
+     * @param quizName the name of the quiz
+     * @param questionId the id of the question
+     * @param response
+     * @return edits a question on a given index in a given quiz
+     */
     @PutMapping("quizzes/{name}/{id}")
-    public String editQuestion(@RequestBody String question, @PathVariable("name") String quizName, @PathVariable("id") int questionId, HttpServletResponse response) {
+    public String editQuestion(@RequestBody String question,
+                               @PathVariable("name") String quizName,
+                               @PathVariable("id") int questionId, HttpServletResponse response) {
         try {
             Quiz quiz = quizPersistence.loadQuiz(quizName);
             quiz.setQuestion(questionId, objectMapper.readValue(question, Question.class));
@@ -91,6 +129,11 @@ public class QuizController {
         return null;
     }
 
+    /**
+     * deletes a quiz
+     * @param quizName the name of the quiz
+     * @param response
+     */
     @DeleteMapping("quizzes/{name}")
     public void deleteQuiz(@PathVariable("name") String quizName, HttpServletResponse response) {
         if (quizPersistence.deleteQuiz(quizName))
@@ -99,9 +142,17 @@ public class QuizController {
             response.setStatus(404);
     }
 
+    /**
+     * deletes a question in a given position from a given quiz
+     * @param quizName the name of the quiz
+     * @param questionId the index of the question
+     * @param response
+     * @return the updated quiz
+     */
     @DeleteMapping("quizzes/{name}/{id}")
     @ResponseBody
-    public String deleteQuestion(@PathVariable("name") String quizName, @PathVariable("id") int questionId, HttpServletResponse response) {
+    public String deleteQuestion(@PathVariable("name") String quizName,
+                                 @PathVariable("id") int questionId, HttpServletResponse response) {
         try {
             Quiz quiz = quizPersistence.loadQuiz(quizName);
             quiz.deleteQuestion(questionId);
