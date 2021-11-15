ad2671ca43032a556418582583ecbbce213a8c4a
oskarjor <oskarjor@stud.ntnu.no>
2021-11-15 13:14:20 +0100

    feat: implement skeleton for LeaderboardPageTest
    implement the skeleton code for testing leaderboard

---
+++ Quiz-app/ui/src/test/java/ui/LeaderboardPageTest.java
@@ -0,0 +1,51 @@
+package ui;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import io.SavePaths;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+
+public class LeaderboardPageTest extends ApplicationTest {
+
+    private FXMLLoader loader;
+
+    private WireMockConfiguration config;
+    private WireMockServer wireMockServer;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        SavePaths.enableTestMode();
+        config = WireMockConfiguration.wireMockConfig().port(8080);
+        wireMockServer = new WireMockServer(config.portNumber());
+        wireMockServer.start();
+        WireMock.configureFor("localhost", config.portNumber());
+        stubFor(get(urlEqualTo("/api/leaderboards"))
+                .willReturn(aResponse()
+                        .withBody("[]")));
+        loader = new FXMLLoader(getClass().getResource("Leaderboard.fxml"));
+        final Parent root = loader.load();
+        wireMockServer.stop();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+
+    @BeforeEach
+    public void startWireMockServerAndSetup() throws IOException, InterruptedException {
+        config = WireMockConfiguration.wireMockConfig().port(8080);
+        wireMockServer = new WireMockServer(config.portNumber());
+        wireMockServer.start();
+        WireMock.configureFor("localhost", config.portNumber());
+    }
+}
