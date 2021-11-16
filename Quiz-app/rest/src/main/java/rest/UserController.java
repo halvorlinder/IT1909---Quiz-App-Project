31a5e1d7322b87ab164f16bb437dbadeccabcfc5
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-11-16 09:43:49 +0100

    docs: write javadoc
    written javadoc for UserController

--- Quiz-app/rest/src/main/java/rest/UserController.java
+++ Quiz-app/rest/src/main/java/rest/UserController.java
@@ -20,18 +20,26 @@ public class UserController {
     private final UserPersistence userPersistence;
     private final ObjectMapper objectMapper;
 
+    /**
+     *
+     * @throws IOException
+     */
     public UserController() throws IOException {
         objectMapper = UserPersistence.createObjectMapper();
         userPersistence = new UserPersistence();
     }
 
-
+    /**
+     * attempts to login in a user
+     * @param response
+     * @param userDataJson the login information
+     */
     @PostMapping("/users/login")
-    public void loginUser(HttpServletResponse response, @RequestBody String userDataJson){
+    public void loginUser(HttpServletResponse response, @RequestBody String userDataJson) {
         try {
             UserData userData = userPersistence.loadUserData();
             System.out.println(userData);
-            if(userData.attemptLogIn(objectMapper.readValue(userDataJson, UserRecord.class)))
+            if (userData.attemptLogIn(objectMapper.readValue(userDataJson, UserRecord.class)))
                 response.setStatus(200);
             else
                 response.setStatus(403);
@@ -41,17 +49,21 @@ public class UserController {
         }
     }
 
+    /**
+     * attempts to register a user
+     * @param response
+     * @param userDataJson the registration information
+     */
     @PostMapping("/users/register")
-    public void registerUser(HttpServletResponse response, @RequestBody String userDataJson){
+    public void registerUser(HttpServletResponse response, @RequestBody String userDataJson) {
         try {
             UserData userData = userPersistence.loadUserData();
             System.out.println(userData);
             UserRecord userRecord = objectMapper.readValue(userDataJson, UserRecord.class);
-            if(userData.attemptRegister(userRecord)){
+            if (userData.attemptRegister(userRecord)) {
                 userPersistence.saveUserData(userData);
                 response.setStatus(200);
-            }
-            else
+            } else
                 response.setStatus(403);
         } catch (IOException e) {
             response.setStatus(500);
