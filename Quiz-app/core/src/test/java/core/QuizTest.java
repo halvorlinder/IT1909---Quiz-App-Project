3cdaa0c24b678b79cfa203c593b8a37cde4dd5bd
oskarjor <oskarjor@stud.ntnu.no>
2021-11-19 19:47:26 +0100

    docs: write javadoc for QuizTest

--- Quiz-app/core/src/test/java/core/QuizTest.java
+++ Quiz-app/core/src/test/java/core/QuizTest.java
@@ -13,7 +13,9 @@ public class QuizTest {
     private Quiz quiz;
     List<Question> questions;
 
-
+    /**
+     * Set up a quiz
+     */
     @BeforeEach
     public void setUp() {
         questions = new ArrayList<>();
@@ -23,6 +25,9 @@ public class QuizTest {
         quiz = new Quiz("quiz101", questions, "hallvard");
     }
 
+    /**
+     * Test the length of the quiz
+     */
     @Test
     public void testQuizLength() {
         Assertions.assertEquals(questionCount, quiz.getQuizLength());
@@ -30,6 +35,9 @@ public class QuizTest {
         Assertions.assertEquals(questionCount + 1, quiz.getQuizLength());
     }
 
+    /**
+     * Test getting questions from the quiz
+     */
     @Test
     public void testGetQuestions() {
         List<Question> actual_questions = quiz.getQuestions();
@@ -38,18 +46,27 @@ public class QuizTest {
         }
     }
 
+    /**
+     * Test the toString method
+     */
     @Test
     public void testToString() {
         String test = quiz.toString();
         Assertions.assertTrue(test.contains("questions=" + quiz.getQuestions()));
     }
 
+    /**
+     * Test deleting questions from the quiz
+     */
     @Test
     public void testDeleteQuestion() {
         quiz.deleteQuestion(0);
         Assertions.assertEquals(questionCount-1, quiz.getQuizLength());
     }
 
+    /**
+     * Test setting a question in the quiz
+     */
     @Test
     public void testSetQuestion() {
         quiz.setQuestion(0, new Question("?", List.of("x", "y", "z", "w"), 3));
@@ -57,23 +74,35 @@ public class QuizTest {
         Assertions.assertEquals(questionCount, quiz.getQuizLength());
     }
 
+    /**
+     * Test creating a quiz without a name
+     */
     @Test
     public void emptyNameQuiz() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Quiz("", List.of(), "per"));
     }
 
+    /**
+     * Test getting questions that don't exist
+     */
     @Test
     public void getNonExistingQuestion() {
         Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> quiz.getQuestion(-1));
         Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> quiz.getQuestion(quiz.getQuizLength()));
     }
 
+    /**
+     * Test deleting questions that don't exist
+     */
     @Test
     public void deleteNonExistingQuestion() {
         Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> quiz.deleteQuestion(-1));
         Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> quiz.deleteQuestion(quiz.getQuizLength()));
     }
 
+    /**
+     * Test setting questions that don't exist
+     */
     @Test
     public void setNonExistingQuestion() {
         Question q = new Question("?", List.of("1", "2", "3", "4"), 0);
@@ -81,8 +110,11 @@ public class QuizTest {
         Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> quiz.setQuestion(quiz.getQuizLength(), q));
     }
 
+    /**
+     * Test setting a question
+     */
     @Test
-    public void putQuestion() {
+    public void setQuestion2() {
         Question q = new Question("?", List.of("a", "b", "c", "d"), 3);
         quiz.setQuestion(0, q);
         Assertions.assertEquals(quiz.getQuestion(0).getQuestion(), q.getQuestion());
@@ -92,6 +124,9 @@ public class QuizTest {
         }
     }
 
+    /**
+     * Test creating a quiz with no creator
+     */
     @Test
     public void testEmptyCreator() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Quiz("test", questions, ""));
