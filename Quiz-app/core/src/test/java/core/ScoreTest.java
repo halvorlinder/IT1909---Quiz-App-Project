5bf11ef8e69b44a91c01dd744be34f040d927a49
oskarjor <oskarjor@stud.ntnu.no>
2021-11-19 19:48:37 +0100

    docs: write javadoc for ScoreTest

--- Quiz-app/core/src/test/java/core/ScoreTest.java
+++ Quiz-app/core/src/test/java/core/ScoreTest.java
@@ -9,11 +9,17 @@ public class ScoreTest {
     private Score score;
     private final String username = "user";
 
+    /**
+     * Test constructor with negative score
+     */
     @Test
     public void testInvalidConstructor(){
         assertThrows(IllegalArgumentException.class, () -> new Score(username, -1));
     }
 
+    /**
+     * Test constructor with valid arguments
+     */
     @Test
     public void testValidConstructor(){
         assertDoesNotThrow(() -> score = new Score(username, 1));
@@ -21,12 +27,15 @@ public class ScoreTest {
         assertEquals(1, score.getPoints());
     }
 
+    /**
+     * Test comparing to score elements
+     */
     @Test
     public void testCompareScores() {
         Score largeScore = new Score(username, 5);
         Score smallScore = new Score(username, 1);
         Score smallScore2 = new Score("notTest", 1);
         assertEquals(4, smallScore.compareTo(largeScore));
-        assertTrue(smallScore.equals(smallScore2));
+        assertEquals(smallScore, smallScore2);
     }
 }
